9c8d8c6bdb08a64f89ea224d41bc9daff7633d71
A.AntorCSE404 <a.antor.cse@gmail.com>
2026-03-05 20:53:40 +0600

    Create Q1.py

---
+++ Loop/Q1.py
@@ -0,0 +1,7 @@
+# use nested loop to solve the question Q1
+for i in range(5,0,-1):
+    for j in range(i):
+        print(i, end=' ')
+    # this print function is called for printing new line after ending the each outer loop.
+    print()
+
