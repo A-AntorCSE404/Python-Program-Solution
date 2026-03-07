700574a2da9aaf8616f6e675d496dc34bb18bf31
A.AntorCSE404 <a.antor.cse@gmail.com>
2026-03-07 17:27:48 +0600

    Add function to sum two numbers with docstring

---
+++ function/Q1.py
@@ -0,0 +1,11 @@
+def add(a, b):
+
+    """Returns the sum of two number a and b"""
+    return a+b
+
+if __name__=="__main__":
+    
+    # print the string doc from the function add.
+    print(add.__doc__)
+    print(add(5,3))
+
